0e1f8c7594066fe3d6af5744350d4559afa79af5
Byron Wall <byron@byroni.us>
2018-03-29 22:39:40 -0400

    Update server code

--- server.ts
+++ server.ts
@@ -12,7 +12,130 @@ const app = express();
 const db = new nedb({ filename: "./data.db", autoload: true });
 let hn = new HackerNewsApi();
 
+db.ensureIndex({ fieldName: "id", unique: true }, function(err) {
+  console.log("nedb unique error", err);
+});
+
 app.use(bodyParser.json());
+
+interface TopStories {
+  items: number[];
+  id: "topstories";
+}
+
+interface ItemExt extends Item {
+  firstLayerOnly?: boolean;
+}
+
+async function _loadFirstLayerOfInfo(ids: number[]) {
+  // have a list of ids that shoudl not be in the database yet
+
+  // iterate through those ids,
+
+  let promises = [];
+
+  let results: ItemExt[] = [];
+
+  ids.forEach(id => {
+    let reqProm = new Promise<ItemExt>((resolve, reject) => {
+      db.findOne<ItemExt>({ id: id }, (err, doc) => {
+        if (doc === null) {
+          // need to load that object
+
+          let newItem = hn.fetchItem(id).then(item => {
+            console.log("item is here!", item);
+            let itemExt = item as ItemExt;
+            itemExt.firstLayerOnly = true;
+
+            // TODO: fully implement the callback here
+            console.log("adding obj to DB");
+            db.insert(itemExt, (err, doc) => {
+              if (err != null) {
+                reject(err);
+              } else {
+                resolve(doc);
+              }
+            });
+          });
+        } else {
+          console.log("obj was in DB");
+          resolve(doc);
+        }
+      });
+    });
+    promises.push(reqProm);
+  });
+
+  console.log("waiting for all requests to come through");
+  return await Promise.all(promises);
+
+  // return a obj with all of the first layer info for these?
+}
+
+app.get("/topstories", (req, res) => {
+  // return a set of 30 stories with the titel, comment count, and URL
+  // add those to the DB and set some flag saying that they need full details loaded
+  // load the first layer and note that more could be loaded
+  // store those top stories for some period of time
+
+  db.findOne<TopStories>({ id: "topstories" }, (err, doc) => {
+    console.log("err", err, "doc", doc);
+
+    if (doc === null) {
+      // go load top stories
+      let topIds = _getTopStories().then(ids => {
+        let topstories: TopStories = { id: "topstories", items: ids };
+        db.insert(topstories);
+
+        // process first layer and then return that obj
+
+        // TODO: split ths out so it works on a second call
+
+        // TODO: load the first layer of info for those stories
+        _loadFirstLayerOfInfo(ids.slice(0, 10)).then(allObj => {
+          console.log("all objects loaded...");
+          res.json(allObj);
+        });
+      });
+    } else {
+      // return those top stories as a clean object
+      _loadFirstLayerOfInfo(doc.items.slice(0, 10)).then(allObj => {
+        console.log("all objects loaded...");
+        res.json(allObj);
+      });
+    }
+  });
+});
+
+interface ItemParams {
+  id: number;
+}
+
+app.get("/item/:id", (req, res) => {
+  // this will return a fully loaded object to be parsed by the app
+  console.log(req.query);
+  console.log(req.params);
+
+  let params: ItemParams = req.params;
+
+  let testId = params.id;
+  // check if the database contains the items
+  db.findOne({ id: testId }, (err, doc) => {
+    console.log("error", err, "doc", doc);
+
+    if (doc === null) {
+      console.log("none was found");
+
+      // go get the document loaded
+    }
+  });
+
+  // if not, go fetch the full item from HN API
+
+  // return the result
+  res.send("it went through");
+});
+
 app.get("/*", (req, res) => {
   console.log("site is running");
 
@@ -27,6 +150,10 @@ function outputItem(item: Item, indent: number) {
   if (item.kidsObj !== undefined) {
     item.kidsObj.forEach(kid => outputItem(kid, indent + 1));
   }
+}
+
+async function _getTopStories() {
+  return await hn.fetchItemIds("topstories");
 }
 
 async function doWork() {
@@ -106,4 +233,4 @@ async function addItemToDb(item: Item) {
 var port = process.env.PORT || 3000;
 app.listen(port);
 
-console.log("server is running on port 80");
+console.log("server is running on port: " + port);
