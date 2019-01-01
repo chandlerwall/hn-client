1a40225d0a1de85752b9d00d8f548b8b1fa41247
Byron Wall <byron@byroni.us>
2019-01-01 11:35:52 -0500

    Add support for Sentry

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,3 @@
-
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -8,6 +7,12 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://d8e8092157294c86b5014343cede60e6@sentry.io/1362584"
+});
 
 ReactDOM.render(
   <BrowserRouter>
