89f3587a3afbc923951121903462906e11a703c3
Chandler Wall <c@swall.us>
2019-01-25 21:57:01 -0600

    Remove build files from server project

--- server/build/helpers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function _getUnixTimestamp() {
-    return Math.floor(new Date().valueOf() / 1000);
-}
-exports._getUnixTimestamp = _getUnixTimestamp;
